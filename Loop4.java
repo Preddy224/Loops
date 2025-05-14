feb2fc3a3ab0002b7880a277e027eeb8e509c543
CHINATL PEERAREDDY <130660537+Preddy224@users.noreply.github.com>
2025-05-14 21:58:37 +0530

    Add files via upload

---
+++ Loop4.java
@@ -0,0 +1,16 @@
+package Loops.blc.elc;
+
+public class Loop4 {
+
+	public static void main(String[] args) {
+		int n=15;
+		for(int i=1; i<=n; i++) {
+			
+			if(i%2 !=0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
